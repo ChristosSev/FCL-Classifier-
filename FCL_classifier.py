eff38e4ec6e530620ff071f499ccd6f1bf54977b
Christos Sevastopoulos <45820335+ChristosSev@users.noreply.github.com>
2022-04-28 17:43:23 -0500

    Create FCL_classifier.py

---
+++ FCL_classifier.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+import torch
+import glob
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.autograd import Variable
+import torchvision
+import pathlib
+import torchvision.models as models
+
+
+#checking for device
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print(device)
+
+#Transforms
+transformer=transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),  #0-255 to 0-1, numpy to tensors
+    transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1] , formula (x-mean)/std
+                        [0.5,0.5,0.5])
+])
+
+#Dataloader
+
+
+
+#the opposite
+
+test_path='/home/christos_sevastopoulos/Desktop/TOY_DATASET/TRAINING'
+train_path= '/home/christos_sevastopoulos/Desktop/TOY_DATASET/TESTING'
+
+
+train_loader=DataLoader(
+    torchvision.datasets.ImageFolder(train_path,transform=transformer),
+    batch_size=64, shuffle=True
+)
+test_loader=DataLoader(
+    torchvision.datasets.ImageFolder(test_path,transform=transformer),
+    batch_size=32, shuffle=True
+)
+
+root=pathlib.Path(train_path)
+classes=sorted([j.name.split('/')[-1] for j in root.iterdir()])
+
+
+print(classes)
+
+
+#calculating the size of training and testing image
+train_count=len(glob.glob(train_path+'/**/*.jpg'))
+test_count=len(glob.glob(test_path+'/**/*.jpg'))
+
+print(train_count,test_count)
+
+
+
+#define the model
+
+model = torchvision.models.resnet18(pretrained = False).to(device)
+
+optimizer=Adam(model.parameters(),lr=0.001,weight_decay=0.0001)
+loss_function=nn.CrossEntropyLoss()
+
+num_epochs=25
+
+
+
+# Model training and saving best model
+
+best_accuracy = 0.0
+
+for epoch in range(num_epochs):
+
+    # Evaluation and training on training dataset
+    model.train()
+    train_accuracy = 0.0
+    train_loss = 0.0
+
+    for i, (images, labels) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            images = Variable(images.cuda())
+            labels = Variable(labels.cuda())
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.cpu().data * images.size(0)
+        _, prediction = torch.max(outputs.data, 1)
+
+        train_accuracy += int(torch.sum(prediction == labels.data))
+
+    train_accuracy = train_accuracy / train_count
+    train_loss = train_loss / train_count
+
+    # Evaluation on testing dataset
+    model.eval()
+
+    test_accuracy = 0.0
+    for i, (images, labels) in enumerate(test_loader):
+        if torch.cuda.is_available():
+            images = Variable(images.cuda())
+            labels = Variable(labels.cuda())
+
+        outputs = model(images)
+        _, prediction = torch.max(outputs.data, 1)
+        test_accuracy += int(torch.sum(prediction == labels.data))
+
+    test_accuracy = test_accuracy / test_count
+
+    print('Epoch: ' + str(epoch) + ' Train Loss: ' + str(train_loss) + ' Train Accuracy: ' + str(
+        train_accuracy) + ' Test Accuracy: ' + str(test_accuracy))
+
+    # Save the best model
+    if test_accuracy > best_accuracy:
+        torch.save(model.state_dict(), 'best_checkpoint.model')
+        best_accuracy = test_accuracy
+
+
+
+
+
+
